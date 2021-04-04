77b78c21279a8cfe55f697038f8555476ffa17a8
shanole <73411129+shanole@users.noreply.github.com>
2021-04-04 10:19:42 -0700

    Courses (#2)
    * add course catalog

--- src/main.js
+++ src/main.js
@@ -2,3 +2,24 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import { Catalog } from './course-catalog.js';
+
+function displayClasses(coursesPicked) {
+  for (let i = 0; i <= 11; i++) {
+    if (coursesPicked === Catalog["courses"][i]["course-title"]) {
+      return Catalog["courses"][i];
+    }
+  }
+}
+
+$(document).ready(function() {
+  $("#submitcourse").click(function(event) {
+    event.preventDefault();
+    $("#formOne").hide();
+    $("#submit").hide();
+
+    const coursePicked = $("input:radio[name=courses]:checked").val();
+    $("#results").show();
+    $(".coursesPicked").text(JSON.stringify(displayClasses(coursePicked)));
+  });
+});
