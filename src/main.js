30739e29e9c77b8ea7b8c65f8bb8ecee02b1900d
tlpackus <tlpackus@gmail.com>
2021-04-05 19:14:46 -0700

    update to include two click functions Co-authored-by: Juan Hasbun <zemenarwow@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,24 +2,37 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import { Catalog } from './js/course-catalog.js';
+// import { Catalog } from './course-catalog.js';
 
-function displayClasses(coursesPicked) {
-  for (let i = 0; i <= 11; i++) {
-    if (coursesPicked === Catalog.courses[i].courseTitle) {
-      return Catalog.courses[i];
-    }
-  }
-}
+// function displayClasses(coursesPicked) {
+//   for (let i = 0; i <= 11; i++) {
+//     if (coursesPicked === Catalog["courses"][i]["course-title"]) {
+//       return Catalog["courses"][i];
+//     }
+//   }
+// }
 
 $(document).ready(function() {
   $("#submitcourse").click(function(event) {
     event.preventDefault();
-    $("#formOne").hide();
+    $("#catalog").hide();
     $("#submit").hide();
 
-    const coursePicked = $("input:radio[name=courses]:checked").val();
+    const name = $("input#name").val();
+    let careerPicked = $("input:radio[name=career]:checked").val();
+    let electivePicked = $("input:radio[name=elective]:checked").val();
+    let langsPicked = [];
+    $("input:checkbox[name=language]:checked").each(function() {
+      const pickLang = $(this).val();
+      langsPicked.push(pickLang);
+    });
+
+    console.log(name);
+    console.log(careerPicked);
+    console.log(electivePicked);
+    console.log(langsPicked);
+
     $("#results").show();
-    $(".coursesPicked").text(JSON.stringify(displayClasses(coursePicked)));
+    //$(".coursesPicked").text(JSON.stringify(displayClasses(coursePicked)));
   });
 });
