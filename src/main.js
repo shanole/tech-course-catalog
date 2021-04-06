e931a1d1feb4a3dc33262688e3036eef57f0cce4
tlpackus <tlpackus@gmail.com>
2021-04-05 20:06:29 -0700

    remove comments Co-authored-by: Juan Hasbun <zemenarwow@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,15 +2,6 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-// import { Catalog } from './course-catalog.js';
-
-// function displayClasses(coursesPicked) {
-//   for (let i = 0; i <= 11; i++) {
-//     if (coursesPicked === Catalog["courses"][i]["course-title"]) {
-//       return Catalog["courses"][i];
-//     }
-//   }
-// }
 
 $("#submitcourse").click(function(event) {
   event.preventDefault();
@@ -29,10 +20,4 @@ $("#submitcourse").click(function(event) {
     const pickLang = $(this).val();
     langsPicked.push(pickLang);
   });
-  
-  console.log(name);
-  console.log(careerPicked);
-  console.log(electivePicked);
-  console.log(langsPicked);
-    //$(".coursesPicked").text(JSON.stringify(displayClasses(coursePicked)));
 });
